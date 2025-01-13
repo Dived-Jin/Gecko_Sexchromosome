412f057ad96dfa31e611d25a0a53c338664d5bda
jin <84110808+Dived-Jin@users.noreply.github.com>
2025-01-13 11:01:36 +0800

    Delete 04.Karotype directory

--- 04.Karotype/example/ANGES/node2.sh
+++
@@ -1,4 +0,0 @@
-date
-export PATH=/ldfssz1/ST_DIVERSITY/PUB/USER/zhouyang/software/python-2.7.18/bin/:$PATH
-/ldfssz1/ST_DIVERSITY/PUB/USER/zhouyang/software/python-2.7.18/bin/python /jdfssz1/ST_EARTH/P18Z10200N0122/zhouyang/userDir//software/anges-1.01/src/MASTER/anges_CAR.py node2 1>node2.log 2> node2.log2
-date
